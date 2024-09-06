9a76f9a429b385cf0e5f55de65078545fc3af1a4
aryanxsrivastava <aryan.srivastava101203@gmail.com>
2024-09-06 18:03:27 +0530

    db

--- utils/schema.jsx
+++ utils/schema.jsx
@@ -1,10 +1,10 @@
 import {
-    integer,
-    numeric,
-    pgTable,
-    serial,
-    varchar,
-    pgSchema,
+  integer,
+  numeric,
+  pgTable,
+  serial,
+  varchar,
+  pgSchema,
 } from "drizzle-orm/pg-core";
 
 // Define your schema
@@ -12,25 +12,25 @@ export const mySchema = pgSchema('my_schema');
 
 // Define your tables within the schema
 export const Budgets = mySchema.table("budgets", {
-    id: serial("id").primaryKey(),
-    name: varchar("name").notNull(),
-    amount: varchar("amount").notNull(),
-    icon: varchar("icon"),
-    createdBy: varchar("createdBy").notNull(),
+  id: serial("id").primaryKey(),
+  name: varchar("name").notNull(),
+  amount: varchar("amount").notNull(),
+  icon: varchar("icon"),
+  createdBy: varchar("createdBy").notNull(),
 });
 
 export const Incomes = mySchema.table("incomes", {
-    id: serial("id").primaryKey(),
-    name: varchar("name").notNull(),
-    amount: varchar("amount").notNull(),
-    icon: varchar("icon"),
-    createdBy: varchar("createdBy").notNull(),
+  id: serial("id").primaryKey(),
+  name: varchar("name").notNull(),
+  amount: varchar("amount").notNull(),
+  icon: varchar("icon"),
+  createdBy: varchar("createdBy").notNull(),
 });
 
 export const Expenses = mySchema.table("expenses", {
-    id: serial("id").primaryKey(),
-    name: varchar("name").notNull(),
-    amount: numeric("amount").notNull().default(0),
-    budgetId: integer("budgetId").references(() => Budgets.id),
-    createdAt: varchar("createdAt").notNull(),
+  id: serial("id").primaryKey(),
+  name: varchar("name").notNull(),
+  amount: numeric("amount").notNull().default(0),
+  budgetId: integer("budgetId").references(() => Budgets.id),
+  createdAt: varchar("createdAt").notNull(),
 });
