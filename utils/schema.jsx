21ed3390d1accaf34e7cf8cf092f137d5e4c64ec
Vardhan-Harsh07 <Vardhanharsh806@gmail.com>
2024-09-06 18:02:43 +0530

    db

--- utils/schema.jsx
+++ utils/schema.jsx
@@ -4,27 +4,33 @@ import {
     pgTable,
     serial,
     varchar,
-  } from "drizzle-orm/pg-core";
-  
-  export const Budgets = pgTable("budgets", {
+    pgSchema,
+} from "drizzle-orm/pg-core";
+
+// Define your schema
+export const mySchema = pgSchema('my_schema');
+
+// Define your tables within the schema
+export const Budgets = mySchema.table("budgets", {
     id: serial("id").primaryKey(),
     name: varchar("name").notNull(),
     amount: varchar("amount").notNull(),
     icon: varchar("icon"),
     createdBy: varchar("createdBy").notNull(),
-  });
-  
-  export const Incomes = pgTable("incomes", {
+});
+
+export const Incomes = mySchema.table("incomes", {
     id: serial("id").primaryKey(),
     name: varchar("name").notNull(),
     amount: varchar("amount").notNull(),
     icon: varchar("icon"),
     createdBy: varchar("createdBy").notNull(),
-  });
-  export const Expenses = pgTable("expenses", {
+});
+
+export const Expenses = mySchema.table("expenses", {
     id: serial("id").primaryKey(),
     name: varchar("name").notNull(),
     amount: numeric("amount").notNull().default(0),
     budgetId: integer("budgetId").references(() => Budgets.id),
     createdAt: varchar("createdAt").notNull(),
-  });
+});
