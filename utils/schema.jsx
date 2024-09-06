222d6e108aed24fdfd5326d8dbdbf5fb32339138
aryanxsrivastava <aryan.srivastava101203@gmail.com>
2024-09-06 18:35:31 +0530

    Revert "db"
    This reverts commit 21ed3390d1accaf34e7cf8cf092f137d5e4c64ec.

--- utils/schema.jsx
+++ utils/schema.jsx
@@ -4,33 +4,27 @@ import {
     pgTable,
     serial,
     varchar,
-    pgSchema,
-} from "drizzle-orm/pg-core";
-
-// Define your schema
-export const mySchema = pgSchema('my_schema');
-
-// Define your tables within the schema
-export const Budgets = mySchema.table("budgets", {
+  } from "drizzle-orm/pg-core";
+  
+  export const Budgets = pgTable("budgets", {
     id: serial("id").primaryKey(),
     name: varchar("name").notNull(),
     amount: varchar("amount").notNull(),
     icon: varchar("icon"),
     createdBy: varchar("createdBy").notNull(),
-});
-
-export const Incomes = mySchema.table("incomes", {
+  });
+  
+  export const Incomes = pgTable("incomes", {
     id: serial("id").primaryKey(),
     name: varchar("name").notNull(),
     amount: varchar("amount").notNull(),
     icon: varchar("icon"),
     createdBy: varchar("createdBy").notNull(),
-});
-
-export const Expenses = mySchema.table("expenses", {
+  });
+  export const Expenses = pgTable("expenses", {
     id: serial("id").primaryKey(),
     name: varchar("name").notNull(),
     amount: numeric("amount").notNull().default(0),
     budgetId: integer("budgetId").references(() => Budgets.id),
     createdAt: varchar("createdAt").notNull(),
-});
+  });
